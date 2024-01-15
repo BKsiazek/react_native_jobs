e06c6be58b113d0a099bdcbf5e39842da18348da
BartekKsiazek <bksiazek15@gmail.com>
2024-01-15 19:34:11 +0100

    First commit.

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text style={{ color: "red" }}>Home 123</Text>
+    </View>
+  );
+};
+
+export default Home;
